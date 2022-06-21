ae11fa3479cc85e8020ebf05b82dab94064fdbd1
kaesdorfm <kaesdorfm@cip.ifi.lmu.de>
2022-06-21 17:04:20 +0100

    restyled the cards, added new button

--- src/components/FungiItem.vue
+++ src/components/FungiItem.vue
@@ -9,13 +9,31 @@ export default {
     season:String,
     foodValue:Number,
     occurrence:String
+  },
+  data(){
+    return{
+      btnClicked:false
+    }
+  },
+  methods:{
+        clickBtn(){
+          console.log(this);
+          this.btnClicked = true;
+        }
+
   }
 }
+
 </script>
 
 <template>
         <div class="card fungi-card">
-          <p class="card-icon">🍄</p> 
+          <div class="card-navbar">
+                <span class="card-icon"><h3>🍄 Fungi {{fungiId}}</h3></span> 
+                <button >❌</button>
+                <button @click="clickBtn()" ref="el">🌂</button>
+          </div>
+          <div class="card-content">
           <table>
             <tr><td class="card-label">Id:</td><td> {{ fungiId }}</td></tr>
             <tr><td class="card-label">Name:</td><td> {{ name }}</td></tr>
@@ -24,11 +42,25 @@ export default {
             <tr><td class="card-label">FoodValue:</td><td> {{ foodValue }}</td></tr>
             <tr><td class="card-label">Occurrence:</td><td> {{ occurrence }}</td></tr>
           </table>
+          </div>
         </div>
 </template>
 
 <style scoped>
+
+.fungi-card > .card-content .hide {
+  display:none;
+}
+
+.clicked{
+  background-color:red;
+}
+
 .fungi-card{
-  background-color:aquamarine;
+  background-color:rgb(255, 196, 190);
+}
+
+.card-navbar{
+    background-color:rgb(253, 153, 144);
 }
 </style>
